797b387a09e6569dca4fb8c67c35ef99e1ac62aa
Phillip Schichtel <phillip.public@schich.tel>
2017-08-08 23:07:45 +0200

    initial version

---
+++ cv.js
@@ -0,0 +1,15 @@
+window.addEventListener('DOMContentLoaded', function() {
+
+    var name = document.getElementById('name').innerHTML;
+    var emailAddress = name.toLowerCase().replace(' ', '@').replace(/(tel)$/, '.$1');
+
+    var emailLinks = document.querySelectorAll('a.email');
+    for (var i = 0; i < emailLinks.length; ++i) {
+        var link = emailLinks.item(i);
+        link.innerHTML = emailAddress;
+        link.addEventListener('click', function(e) {
+            e.preventDefault();
+            window.location.href = 'mailto:' + emailAddress;
+        });
+    }
+});
